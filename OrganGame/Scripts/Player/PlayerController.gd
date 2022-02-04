5fb590fddcc008b5a89ba6d9ebedb1d8b055b642
Brock Blanck <blanckbrock838@gmail.com>
2022-02-04 15:25:17 -0600

    Players don't collide and Damage reactionfixed

---
+++ OrganGame/Scripts/Player/PlayerController.gd
@@ -0,0 +1,56 @@
+extends Node
+
+enum PlayerState { Idle, Walking, Jumping, Damaged }
+var current_state = PlayerState.Idle
+
+# Movement Variables
+export var speed := 600.0
+export var in_air_speed := 50.0
+export var max_speed_offset := 50.0
+var velocity = Vector2.ZERO
+
+# Jump Variables
+export var jump_strength := 2000.0
+export var gravity := 4500.0
+
+# animation variables
+onready var anim := $AnimatedSprite
+
+# damage variables
+onready var timer := $DamageTimer
+
+func _ready():
+	pass# set_state(MovementState.Idle)
+	
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	
+	match current_state:
+		PlayerState.Idle:
+			pass
+	
+
+# sets the velocity
+func set_velocity(new_velocity : Vector2) -> void:
+	velocity = new_velocity
+# adds the Vector2 argument to the velocity variable
+func change_velocity(mod_velocity : Vector2) -> void:
+	velocity += mod_velocity
+
+# sets the current state and calls the handle state function
+func set_state(new_state) -> void:
+	current_state = new_state
+	handle_state_change()
+# determines the logic during a state change
+func handle_state_change() -> void:
+	match current_state:
+		PlayerState.Jumping:
+			set_velocity(Vector2(velocity.x, -jump_strength))
+			anim.play("JumpFull")
+		PlayerState.Walking:
+			anim.play("Walk")
+		PlayerState.Idle:
+			anim.play("Idle")
+		PlayerState.Damaged:
+			anim.play("Damaged")
+			anim.set_frame(0)
--- OrganGame/Scripts/Player/PlayerMovement.gd
+++ OrganGame/Scripts/Player/PlayerMovement.gd
@@ -1,7 +1,5 @@
 extends KinematicBody2D
 
-var react_damage := false
-
 # State variables
 enum MovementState{ Idle, Walking, Jumping, Falling, JumpCancelled, Damaged }
 var current_state = MovementState.Idle
@@ -20,38 +18,37 @@ export var jump_strength := 2000.0
 export var gravity := 4500.0
 
 # animation variables
-onready var anim := get_node("AnimatedSprite")
+onready var anim := $AnimatedSprite
 
 # damage variables
-onready var timer := get_node("DamageTimer")
+onready var timer := $DamageTimer
+
 
 func _ready():
 	set_state(MovementState.Idle)
-	anim.stop()
 
 func _physics_process(delta):
-	# calculate horizontal direction based on player input
-	# -1 when moving left 1 when moving right
-	var horizontal_direction = (
-		Input.get_action_strength("move_right")
-		- Input.get_action_strength("move_left")
-		)
-		
-	# if player is on ground, horizontal velocity is directly set to speed * direction
-	if is_on_floor():
-		set_velocity(Vector2(horizontal_direction * speed, 0))
-	# if player is in air, horizontal speed changes gradually
-	else:
-		change_velocity(Vector2(horizontal_direction * jump_speed, 0))
-		# clamps the velocity so the player can't move too fast
-		velocity.x = clamp(velocity.x, -speed - max_speed_offset, speed + max_speed_offset)
-		
-		
 	# gravity
 	change_velocity(Vector2(0, gravity * delta))
 	
-	# state change
 	if current_state != MovementState.Damaged:
+		# calculate horizontal direction based on player input
+		# -1 when moving left 1 when moving right
+		var horizontal_direction = (
+			Input.get_action_strength("move_right")
+			- Input.get_action_strength("move_left")
+			)
+			
+		# if player is on ground, horizontal velocity is directly set to speed * direction
+		if is_on_floor():
+			set_velocity(Vector2(horizontal_direction * speed, 0))
+		# if player is in air, horizontal speed changes gradually
+		else:
+			change_velocity(Vector2(horizontal_direction * jump_speed, 0))
+			# clamps the velocity so the player can't move too fast
+			velocity.x = clamp(velocity.x, -speed - max_speed_offset, speed + max_speed_offset)
+		
+		# state change
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			set_state(MovementState.Jumping)
 		elif velocity.y < 0 and Input.is_action_just_released("jump"):
@@ -63,14 +60,10 @@ func _physics_process(delta):
 		elif is_zero_approx(velocity.x) and is_on_floor():
 			set_state(MovementState.Idle)
 		
-	if react_damage:
-		set_velocity(Vector2(1000, -1000))
-		react_damage = false
-	
-	if horizontal_direction < 0 and scale.x != -1:
-		anim.set_flip_h(true)
-	elif horizontal_direction > 0:
-		anim.set_flip_h(false)
+		if horizontal_direction < 0 and scale.x != -1:
+			anim.set_flip_h(true)
+		elif horizontal_direction > 0:
+			anim.set_flip_h(false)
 	
 	# moves player with move_and_slide and sets velocity to the calculated velocity
 	set_velocity(move_and_slide(velocity, UP_DIRECTION))
@@ -99,15 +92,14 @@ func handle_state():
 		MovementState.Idle:
 			anim.play("Idle")
 		MovementState.Damaged:
+			set_velocity(Vector2(1000, -2000))
 			anim.play("Damage")
 			anim.set_frame(0)
 			
 func _on_HurtBox_area_entered(_area):
 	set_state(MovementState.Damaged)
-	react_damage = true
 	timer.start()
 
 
 func _on_DamageTimer_timeout():
 	set_state(MovementState.Idle)
-	react_damage = false
